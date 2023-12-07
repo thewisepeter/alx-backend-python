5678445abdb29b00b2b0d4e6eb2c85f63f5fce94
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 22:13:11 +0300

    solution to 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 ''' type-annotated function to_kv '''
 
+from typing import Union, Tuple
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
         function that takes a string and an int OR float
         returns a tuple.
