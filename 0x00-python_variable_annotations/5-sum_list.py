6198be0e2fa0b0043853ee7edf247ded69cffbba
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 20:51:32 +0300

    solution to 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 ''' definitions with set values '''
 
-type Input_list = list[float]
+from typing import List
 
 
-def sum_list(input_list: Input_list) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''
         function which takes a list input_list
         of floats as argument and returns
