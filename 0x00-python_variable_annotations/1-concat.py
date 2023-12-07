397684b1ec008322c8a1192d06650473a9cdaea9
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 18:56:13 +0300

    solution to 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' type-annotated function concat that combines 2 strings '''
+
+
+def add(str1: str, str2: str) -> str:
+    ''' function that concatenates 2 string '''
+    return str1 + str2
