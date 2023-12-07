48f39fa7d35213693108b243c0069997a3a1164a
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 19:07:57 +0300

    solution to 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function floor '''
+import math
+
+
+def floor(n: float) -> int:
+    '''
+        function which takes a float
+        as argument and returns the
+        floor of the float
+    '''
+    return math.floor(n)
