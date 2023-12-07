056860c6045de417b782f587db2454d71fe1a08d
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 23:07:47 +0300

    solution to 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 ''' type-annotated function make_multiplier'''
 
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     ''' returns list of tuples'''
     return [(i, len(i)) for i in lst]
