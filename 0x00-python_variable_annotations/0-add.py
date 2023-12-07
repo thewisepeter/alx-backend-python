873077b0509b21713073b7e3ef5574c1dc86851a
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 18:48:46 +0300

    solution to 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' type-annotated function add that takes a float a and a float b '''
+
+
+def add(a: float, b: float) -> float:
+    ''' function that adds 2 floats '''
+    return a + b
