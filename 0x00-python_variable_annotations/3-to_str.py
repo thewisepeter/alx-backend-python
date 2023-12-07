e6d3412c2da12aa444c77c158f45a3ea34b7e394
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 19:13:08 +0300

    solution to 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function to_str '''
+import math
+
+
+def to_str(n: float) -> str:
+    '''
+        function which takes a float
+        as argument and returns the
+        string representation
+    '''
+    return str(n)
