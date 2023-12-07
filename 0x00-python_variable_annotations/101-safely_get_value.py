b8132d0cb802cc46792253bf00286faca8da8691
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-07 23:35:11 +0300

    solution to 101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+''' type-annotated function '''
+
+from typing import Mapping, Any, Union, TypeVar
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default:
+                     Union[T, None] = None) -> \
+            Union[Any, T]:
+    ''' Duck Typing '''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
