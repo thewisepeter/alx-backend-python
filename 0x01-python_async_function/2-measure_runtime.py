db1440bf00b730d011f966d846521aec84ff705a
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-11 17:27:12 +0300

    solution to 3-tasks.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,7 +14,7 @@
 
 import asyncio
 import time
-wait_random = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
