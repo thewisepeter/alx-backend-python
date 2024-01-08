5f7125dedb43b826b95541a056e1bdbcf91f22e4
thewisepeter <rempeterwisdom@gmail.com>
2024-01-08 20:57:49 +0300

    Solution to test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+    tests for utils
+'''
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''
+        various test cases for utils.access_nested_map
+    '''
+    def test_access_nested_map(self):
+        nested_map = {"a": {"b": {"c": 1}}}
+        self.assertEqual(access_nested_map(nested_map, ["a", "b", "c"]), 1)
